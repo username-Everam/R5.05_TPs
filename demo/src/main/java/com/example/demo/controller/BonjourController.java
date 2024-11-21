ec6f4ff4d9c7bc52df86efceb4883d80875b8c26
username-Everam <simsimi.armand@gmail.com>
2024-11-21 15:18:17 +0100

    hello-world

--- demo/src/main/java/com/example/demo/controller/BonjourController.java
+++ demo/src/main/java/com/example/demo/controller/BonjourController.java
@@ -1,16 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("bonjour")
+@RestController
 public class BonjourController {
 
-    @GetMapping
-    public String accueil(Model model) {
-        return "bonjour";
+    @GetMapping("/bonjour")
+    public String accueil() {
+        return "test";
     }
 }
 
